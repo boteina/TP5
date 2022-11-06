dae8835ee5ffa5985e506eb787e9228a3f773815
boutheina <88008969+boteina@users.noreply.github.com>
2022-11-06 22:08:06 +0100

    Add files via upload

---
+++ TP5_EXO3_QST1.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+function searchFull(filename, text) {
+  return new Promise((resolve) => {
+    const regEx = new RegExp(text, "i");
+    const result = [];
+    filename.forEach((item) =>
+      fs.readFile("./" + item + ".txt", "utf8", function (err, contents) {
+        var co = 0;
+        let lines = contents.toString().split("\n");
+        lines.forEach((line) => {
+          if (line && line.search(regEx) >= 0) {
+            console.log("found in file ", item);
+            result.push(line);
+            var co = +1;
+          }
+        });
+
+        resolve(result);
+        console.log("finished");
+      })
+    );
+  });
+}
+var filesarray = ["put your files that are in the same directory"];
+searchFull(filesarray, "the expression");
